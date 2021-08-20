4491f55f5dcfdadd03ec333bd3858b72e9c71ca6
dnjoe96 <donjoedbest@gmail.com>
2021-08-20 15:35:41 +0100

    Set Up User Login
    I added views for users to login into their Dashboard.
    - login
    - logout
    - reset password

--- connect/settings.py
+++ connect/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
